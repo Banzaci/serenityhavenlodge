92338c5ee08871fd4a2d963648960eef5c2568fe
Michel Larsson <>
2023-11-25 16:21:35 +0100

    Structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Rooms = React.lazy(() => import('./pages/Rooms'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/rooms" element={<Rooms />} />
+        </Routes>
+      </React.Suspense>
+      <Footer />
+    </Router>
   );
 }
 
